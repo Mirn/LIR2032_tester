16d2f0d8ae1a29dd5679c8ddbe3750d1812f8fcc
Mirn <sj21dsl@gmail.com>
2016-12-25 19:42:51 +0400

    multi voltage threshold calculation

--- src/main.c
+++ src/main.c
@@ -112,9 +112,9 @@ void main(void)
 			for (uint32_t pos = 0; pos < 8; pos++)
 				stat_print(&lir_workers[pos].info_stats);
 
-			printf("\r\nRESTART\r\n\r\n");
-			for (uint32_t pos = 0; pos < 8; pos++)
-				lir_ctrl[pos] = lir_worker_init(&(lir_workers[pos]), lir_names[pos]);
+//			printf("\r\nRESTART\r\n\r\n");
+//			for (uint32_t pos = 0; pos < 8; pos++)
+//				lir_ctrl[pos] = lir_worker_init(&(lir_workers[pos]), lir_names[pos]);
 		}
 
 
